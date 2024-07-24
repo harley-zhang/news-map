cf0dea7237149580f3c88b118b32d0bbfd864f1c
harley-zhang <harleyzgolego@gmail.com>
2024-07-23 23:15:39 -0400

    updated some text

--- src/components/CtrlPanel.jsx
+++ src/components/CtrlPanel.jsx
@@ -1,10 +1,10 @@
 const CtrlPanel = () => {
-  return (
-    <div className="fixed top-40 left-5 bottom-5 w-[300px] bg-white p-4 shadow-md rounded-lg z-20">
-      <h1 className="text-xl font-bold mb-2">Welcome</h1>
-      <p>Lorem Ipsum</p>
-    </div>
-  );
+    return (
+        <div className="fixed top-40 left-5 bottom-5 w-[300px] bg-white p-4 shadow-md rounded-lg z-20">
+            <h1 className="text-xl font-bold mb-2">View stories</h1>
+            <p>This tool is designed for journalists, so you can understand the local news coverage distribution. We currently have XXX articles from XXXX publishers, ranging from XXX to XXXX.</p>
+        </div>
+    );
 };
 
 export default CtrlPanel;
