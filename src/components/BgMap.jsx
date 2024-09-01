ce38a6d8a00af9a162f16db4440f8899dab33979
harley-zhang <harleyzgolego@gmail.com>
2024-09-01 10:10:54 -0400

    Update BgMap.jsx

--- src/components/BgMap.jsx
+++ src/components/BgMap.jsx
@@ -4,7 +4,6 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import ZCTA from '../assets/filtered_zcta_2020.json';
 import SAMPLE_DATA from '../assets/locations_20240821_104317.geojson';
-import AGGREGATED_DATA from '../assets/aggregated_locations.geojson';
 import CSV_DATA from '../assets/sample_article_output.csv';
 
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiaGFybGV5emhhbmciLCJhIjoiY2x5ejBmeGwxMHMzNzJpb3JwYjhhYzV2NiJ9.mJ4BLWUqkmS4yyV1pg9H-w';
@@ -140,16 +139,6 @@ const BgMap = ({ showLayer }) => {
         {showLayer === 'aggregated-data' && (
           <>
             <Source id="zcta" type="geojson" data={ZCTA}>
-              <Layer
-                id="zcta-layer"
-                type="line"
-                paint={{
-                  'line-color': '#888888',
-                  'line-width': 0.5,
-                }}
-              />
-            </Source>
-            <Source id="zcta" type="geojson" data={AGGREGATED_DATA}>
               <Layer
                 id="zcta-layer"
                 type="line"
